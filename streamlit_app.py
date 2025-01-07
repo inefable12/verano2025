ad286603992bf084c551a1fedd1eb5a4af96a97e
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-01-07 18:25:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,8 +27,9 @@ def pagina1():
                                    "Planteamiento de ecuaciones y programación con calculadora CASIO."]}))
   
   st.subheader("Anotaciones del Martes 7 de enero del 2025")
-  st.write("Manual de la Calculadora CASIO: https://www.manual.pe/casio/cfx-9850gc-plus/manual?p=405#google_vignette")
-  st.write("Ejecutable: https://drive.google.com/open?id=0By0zSAlbksYOZWstVkRLdE5ydjA")
+  st.write("Manual de la Calculadora CASIO:")
+  st.write("https://www.manual.pe/casio/cfx-9850gc-plus/manual?p=405#google_vignette")
+  st.write("Descarga gratuita: https://drive.google.com/open?id=0By0zSAlbksYOZWstVkRLdE5ydjA")
   image1 =Image.open("imagenes/casio_imagen.PNG")
   st.image(image1, caption='Calculadora CASIO')
 
