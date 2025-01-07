cf4640f30cb973935d3ffe34651f79e1835e622d
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-01-07 17:52:45 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,52 +14,64 @@ def Home():
   
 def pagina1():
   st.title("Conceptos Básicos: Física, Química y Matemáticas")
-  
   st.info('Semana 1: 7, 9 y 11 de enero del 2025') 
-  st.write(pd.DataFrame({'Tema': ['Física',
+  st.write(pd.DataFrame({'Curso': ['Física',
                                   'Química',
                                   'Matemáticas'],
-                         'Fecha': ["Introducción a las magnitudes físicas y vectoriales, unidades SI y conversión de unidades.",
+                         'Tema': ["Introducción a las magnitudes físicas y vectoriales, unidades SI y conversión de unidades.",
                                    "Estructura del átomo, configuración electrónica. Impresión 3D de moléculas",
                                    "Planteamiento de ecuaciones y programación con calculadora CASIO."]}))
 #st.header("Introducción")
 
 def pagina2():
- 
-    "Semana 2": {
-        "Física": "Cinemática: movimiento rectilíneo uniforme y uniformemente acelerado.",
-        "Química": "Tabla periódica: grupos y periodos, propiedades periódicas.",
-        "Matemáticas": "Introducción a la resolución de problemas usando Python."
-    }
+  st.title("Temas introductorios de Física, Química y Matemáticas")
+  st.info('Semana 2: 14, 16 y 18 de enero del 2025') 
+  st.write(pd.DataFrame({'Curso': ['Física',
+                                  'Química',
+                                  'Matemáticas'],
+                         'Tema': ["Cinemática: movimiento rectilíneo uniforme y uniformemente acelerado.",
+                                   "Tabla periódica: grupos y periodos, propiedades periódicas.",
+                                   "Introducción a la resolución de problemas usando Python."]}))
 
 def pagina3():
-    "Semana 3": {
-        "Física": "Dinámica: leyes de Newton.",
-        "Química": "Enlaces químicos: iónicos y covalentes.",
-        "Matemáticas": "Álgebra básica: ecuaciones lineales y sistemas de ecuaciones."
-    }
+  st.title("Temas introductorios de Física, Química y Matemáticas")
+  st.info('Semana 3: 21, 23 y 25 de enero del 2025') 
+  st.write(pd.DataFrame({'Curso': ['Física',
+                                  'Química',
+                                  'Matemáticas'],
+                         'Tema': ["Dinámica: leyes de Newton.",
+                                   "Enlaces químicos: iónicos y covalentes.",
+                                   "Álgebra básica: ecuaciones lineales y sistemas de ecuaciones"]}))
 
 def pagina4():
-    "Semana 4": {
-        "Física": "Trabajo, energía y potencia. Concepto de energía cinética y potencial.",
-        "Química": "Reacciones químicas: balanceo y tipos de reacciones.",
-        "Matemáticas": "Geometría: áreas, perímetros y volúmenes de figuras comunes."
-    }
+  st.title("Temas introductorios de Física, Química y Matemáticas")
+  st.info('Semana 4: 28 y 30 de enero; 1 de febrero del 2025') 
+  st.write(pd.DataFrame({'Curso': ['Física',
+                                  'Química',
+                                  'Matemáticas'],
+                         'Tema': ["Trabajo, energía y potencia. Concepto de energía cinética y potencial.",
+                                   "Reacciones químicas: balanceo y tipos de reacciones.",
+                                   "Geometría: áreas, perímetros y volúmenes de figuras comunes."]}))
 
 def pagina5():
-    "Semana 5": {
-        "Física": "Conservación de la energía y cantidad de movimiento.",
-        "Química": "Soluciones: concentración, molaridad, preparación de soluciones.",
-        "Matemáticas": "Funciones: conceptos básicos y representación gráfica."
-    }
+  st.title("Temas introductorios de Física, Química y Matemáticas")
+  st.info('Semana 5: 4, 6 y 8 de febrero del 2025') 
+  st.write(pd.DataFrame({'Curso': ['Física',
+                                  'Química',
+                                  'Matemáticas'],
+                         'Tema': ["Conservación de la energía y cantidad de movimiento.",
+                                   "Soluciones: concentración, molaridad, preparación de soluciones.",
+                                   "Funciones: conceptos básicos y representación gráfica."]}))
 
 def pagina6():
-    "Semana 6": {
-        "Física": "Gravitación y Leyes de Kepler.",
-        "Química": "Ácidos y bases: pH, pOH y neutralización.",
-        "Matemáticas": "Estadística descriptiva: media, mediana y moda."
-    }
-
+  st.title("Temas introductorios de Física, Química y Matemáticas")
+  st.info('Semana 6: 11, 13 y 15 de febrero del 2025') 
+  st.write(pd.DataFrame({'Curso': ['Física',
+                                  'Química',
+                                  'Matemáticas'],
+                         'Tema': ["Gravitación y Leyes de Kepler",
+                                   "Ácidos y bases: pH, pOH y neutralización.",
+                                   "Estadística descriptiva: media, mediana y moda."]}))
 
 ################################################################### 
 ##########################Configuracion############################    
