1eb298108ce1db2cf6f5f2d4b0d1ebd0db0bcfc7
Jesus_Alvarado-Huayhuaz <inefable12@gmail.com>
2025-01-09 07:48:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,7 +24,7 @@ def pagina1():
                                   'Matemáticas'],
                          'Tema': ["Introducción a las magnitudes físicas y vectoriales, unidades SI y conversión de unidades.",
                                    "Estructura del átomo, configuración electrónica. Impresión 3D de moléculas",
-                                   "Planteamiento de ecuaciones y programación con calculadora CASIO."]},hide_index=True))
+                                   "Planteamiento de ecuaciones y programación con calculadora CASIO."]}),hide_index=True)
 
   st.subheader("Anotaciones del Jueves 9 de enero del 2025")
   st.write("Libros, diseño de moléculas e impresión 3D")
